ea35bc95da3551b47d858442dbd9289259c18b41
Himanshu Shetty <149948316+himanshukt03@users.noreply.github.com>
2024-06-25 09:03:29 +0530

    Update routes.js

--- src/route/routes.js
+++ src/route/routes.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router = express.Router()
-const { createStudent } = require('../controller/studentControl');
+const { createStudent,login } = require('../controller/studentControl');
 const { createFeedback, getFeedback} = require('../controller/feedbackControl');
+const {authorization} = require("../middleware/middleware")
 
 router.get('/', (req,res)=>{
     res.send('Router Method!');
 });
 
+//student route
 router.post('/student', createStudent)
-router.post('/feedback')
+router.post('/login' , login)
 
 //feedback route
-router.post('/feedback', createFeedback)
+router.post('/feedback', authorization , createFeedback)
 router.post('/feedback', getFeedback)
 
 module.exports = router;
